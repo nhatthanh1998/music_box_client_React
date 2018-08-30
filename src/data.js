bac6fca190ba900d1e602ff4bf9f956796c9240f
Nhat Thanh <nhatthanhlolo1@gmail.com>
2018-08-30 09:49:51 +0700

    Initial

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    '「 ロミオとシンデレラ 」Romeo and Cinderella by Hanatan ー Lyrics',
+    '【HD】After Dark - Aimer - After Rain【中日字幕】',
+    '【HD】Binary StarCage - SawanoHiroyuki[nZk]Tielle - Cage【中英字幕】',
+    '【HD】Fatehollow ataraxia - Aimer - broKen NIGHT【中日字幕】',
+    '【HD】銀河英雄傳說Die Neue These 邂逅 - SawanoHiroyuki[nZk]Uru - Binary Star【中英字幕】',
+    'Aimer - Brave Shine',
+    'Aimer - Last Stardust FULL',
+    'Aimer - Ninelie',
+    '【HD】After Dark - Aimer - words【中日字幕】',
+    '【HD】After Dark - Aimer - ポラリス Polaris【中日字幕】',
+    '【HD】Fatehollow ataraxia - Aimer - broKen NIGHT【中日字幕】',
+    '【HD】銀河英雄傳說Die Neue These 邂逅 - SawanoHiroyuki[nZk]Uru - Binary Star【中英字幕】',
+    'Aimer - Anatanideawanakereba -Kasetsutouka-',
+    'Aimer - Choucho Musubi',
+]
+
